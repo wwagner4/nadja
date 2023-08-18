9993817f6df075bcb667f41915ddbdac4c2d445a
wwagner4 <wwagner4@gmail.com>
2023-08-18 12:54:14 +0200

    exec in method

--- nadja/src/main/scala/Main.scala
+++ nadja/src/main/scala/Main.scala
@@ -1,8 +1,16 @@
-import sys.process._
+import sys.process.Process
 
-@main def hello: Unit = {
-  val cmd = "ls -l"
-  val r  = cmd.!
-  if r != 0 then throw RuntimeException(s"Could not execute $cmd")
-  println("Success")
+object Main {
+
+  @main def hello: Unit = {
+    val cmdList = List("ls", "-l")
+    exe(cmdList)
+    println("Success")
+  }
+
+  def exe(cmdList: List[String]) = {
+    val p = Process(cmdList)
+    val r  = p.run()
+    if r.exitValue() != 0 then throw RuntimeException(s"Could not execute $cmdList")
+  }
 }
