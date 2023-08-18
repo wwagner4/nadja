3d1d85d24fd6c9573fb6fb62cc8c25b7f0a23ce8
wwagner4 <wwagner4@gmail.com>
2023-08-18 13:27:22 +0200

    list filenames OK

--- nadja/src/main/scala/Main.scala
+++ nadja/src/main/scala/Main.scala
@@ -1,16 +1,38 @@
 import sys.process.Process
 
+import scala.collection.JavaConverters._
+import java.nio.file.{Path, Files}
+
+enum NChar {
+  case N, A, D, J,
+}
+
+case class NFilename(
+  name: String,
+  char: NChar,
+  ext: String
+)
+
 object Main {
 
   @main def hello: Unit = {
-    val cmdList = List("ls", "-l")
-    exe(cmdList)
+    val root = Path.of("/home/wwagner4/work/nadja/kimi2-t1")
+    println(s"root: $root")
     println("Success")
+    Files.list(root)
+      .iterator
+      .asScala
+      .map(p => p.getFileName())
+      .foreach(x => println(x))
   }
 
   def exe(cmdList: List[String]) = {
     val p = Process(cmdList)
     val r  = p.run()
     if r.exitValue() != 0 then throw RuntimeException(s"Could not execute $cmdList")
+  }
+
+  def parseFilename(filename: String): NFilename = {
+    NFilename("", NChar.N, "") 
   }
 }
