9169a4bfa82f92acfb3a0c6326f9ac01893bc28e
wwagner4 <wwagner4@gmail.com>
2023-08-20 15:16:02 +0200

    multiple rows OK

--- nadja/src/main/scala/Main.scala
+++ nadja/src/main/scala/Main.scala
@@ -22,20 +22,31 @@ case class NFilename(
 object Main {
 
   @main def hallo: Unit = {
-    val root = os.home / "work" / "nadja" / "kimi2-t1" / "t1200"
+    mainMontage()
+  }
+
+  def mainResize() = {
+    val root1 = os.home / "work" / "nadja" / "kimi2-t1"
+    val t500 = root1 / "t100"
+    os.makeDir.all(t500)
+    resizeAll(root1, 500, t500)
+  }
+
+  def mainMontage() = {
+    val root = os.home / "work" / "nadja" / "kimi2-t1"
     val out = os.home / "work" / "nadja" / "kimi2-t1" / "out"
     os.makeDir.all(out)
-    println(s"root: $root")
-    val base = Util.createBase(root)
-
-    val infiles = fienames(base, nadja)
-    val t1 = os.temp()
-    val outfile = out / "out006.jpg"
-  
-    montage(infiles, rows=4, cols=2, outfile=t1)
-    resize(t1, outfile)
-
-
+    (1 to 8).foreach {rows =>
+      val size = Util.sizeFromRows(rows)
+      val sizedRoot = os.temp.dir()
+      resizeAll(root, size, sizedRoot)
+      val base = Util.createBase(sizedRoot)
+      val infiles = fienames(base, nadja)
+      val outfile = out / s"out008_${rows}.jpg"
+      val t1 = os.temp()
+      montage(infiles, rows=rows, cols=5, size=size, outfile=t1)
+      resize(t1, outfile)
+    }
   }
 
   def resize(infile: os.Path, outfile: os.Path) = {
@@ -62,8 +73,33 @@ object Main {
     Util.exe(cmd1)
   }
 
-  def montage(infiles: List[os.Path], rows: Int, cols: Int, outfile: os.Path) = {
-    val tilesgeo = s"${rows}x${cols}"
+  val imageExts = Seq("jpg", "jpeg", "png")
+
+
+  def resizeAll(indir: os.Path, size: Int, outdir: os.Path) = {
+
+    val geo = s"${size}x${size}"
+
+    def rs(f: os.Path) = {
+      val cmd = Seq(
+        "convert",
+        f.toString(),
+        "-resize",
+        geo,
+        (outdir / f.last).toString()
+      )
+      println(cmd.mkString(" \\\n"))
+      Util.exe(cmd)
+    }
+
+    os.list(indir)
+      .filter(p => os.isFile(p) && imageExts.contains(p.ext.toLowerCase()))
+      .foreach(rs(_))
+  }
+
+  def montage(infiles: Iterable[os.Path], rows: Int, cols: Int, size: Int, outfile: os.Path) = {
+    val tilesgeo = s"${cols}x${rows}"
+    val fnams = infiles.take(rows * cols).map(_.toString())
     val cmd = List(
       "montage", 
       "-fill", 
@@ -73,17 +109,17 @@ object Main {
       "-bordercolor", 
       "black", 
       "-resize",
-      "1000x",
+      s"${size}x",
       "-gravity",
       "east",
-      "-borderwidth", 
+      "-borderwidth",
       "0", 
       "-tile", 
       tilesgeo, 
       "-geometry", 
       "+0+0",
     ) ++
-    infiles.map{_.toString()} ++
+    fnams ++
     List(
       s"${outfile}",
     )
@@ -93,15 +129,26 @@ object Main {
   }
 
 
-  def fienames(base: NBase, chars: List[NChar]): List[os.Path] = {
-    chars
+  def fienames(base: NBase, chars: List[NChar]): LazyList[os.Path] = {
+    val cs = chars
       .flatMap {c =>  base.files.filter{f => f.char == c}}
       .map(f => Util.path(f, base.path))
+    LazyList.continually(cs).flatten
   }
 
 }
 
 object Util {
+
+  def sizeFromRows(rows: Int): Int = {
+    if rows < 1 then throw IllegalArgumentException(s"Illegal value for sizeFromRows ${rows}")
+    rows match {
+      case 1 => 1200
+      case 2 => 600
+      case 3 => 400
+      case _ => 200
+    }
+  }
 
   def createBase(path: os.Path): NBase = {
 
@@ -114,7 +161,7 @@ object Util {
     )
   }
 
-  def exe(cmdList: List[String]) = {
+  def exe(cmdList: Seq[String]) = {
     val p = Process(cmdList)
     val r  = p.run()
     if r.exitValue() != 0 then throw RuntimeException(s"Could not execute $cmdList")
