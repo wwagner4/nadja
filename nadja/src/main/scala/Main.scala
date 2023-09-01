4f4333900c0e14efc84ed37cc538eb23233d5e47
wwagner4 <atnebel2323>
2023-09-01 14:06:01 +0200

    refactored into scale and draw

--- nadja/src/main/scala/Main.scala
+++ nadja/src/main/scala/Main.scala
@@ -84,35 +84,43 @@ object Main {
       createNImage(base, fn)
     }
 
-    for i <- (0 to 100) do {
+    for i <- (0 to 10) do {
       val canvas = createCanvas(config.width, config.height)
-      scaleAndDrawImage(canvas,
-        Random.shuffle(images).head,
-        Random.nextDouble(),
-        Random.nextDouble(),
-        0.2)
+      val image = Random.shuffle(images).head
+      val scaled = scaleImage(image, 400.0 + Random.nextDouble() * 200)
+      drawImage(canvas, scaled, Random.nextDouble(), Random.nextDouble())
       val fileName = f"a_${i}%05d.jpg"
       val file = t1 / fileName
       val f1 = file.toIO
       val r = ImageIO.write(canvas, "jpg", f1)
       println(s"wrote to ${f1} ${r}")
     }
-    Util.video(t1, 10, config.width, config.height, outfile)
-    println(s"Wrote video to ${outfile}")
+    // Util.video(t1, 10, config.width, config.height, outfile)
+    //println(s"Wrote video to ${outfile}")
   }
 
-  def scaleAndDrawImage(canvas: BufferedImage, image: NImage, xpos: Double, ypos: Double, size: Double) = {
+  def scaleImage(image: NImage, width: Double): BufferedImage = {
+    val iw = image.bufferedImage.getWidth()
+    val _fw = width / iw
+    val ih = image.bufferedImage.getHeight()
+    println(s":: scale: ${width} ${_fw}")
     val at = AffineTransform()
-    val _fw = (canvas.getWidth() * size) / image.bufferedImage.getWidth()
-    val _xpos = canvas.getWidth() * (xpos * size)
-    val _ypos = canvas.getHeight() * (ypos * size)
-    println(s"scale to ${_fw} pos: ${_xpos} ${_ypos}")
     at.scale(_fw, _fw)
     val op = AffineTransformOp(at, AffineTransformOp.TYPE_BICUBIC)
-    val dest = BufferedImage(image.bufferedImage.getWidth(), image.bufferedImage.getHeight(), image.bufferedImage.getType)
+    val dest = BufferedImage((iw * _fw).toInt, (ih * _fw).toInt, image.bufferedImage.getType)
     op.filter(image.bufferedImage, dest)
+    dest
+  }
+
+  def drawImage(canvas: BufferedImage, image: BufferedImage, xpos: Double, ypos: Double) = {
+    val cw = canvas.getWidth()
+    val x1 = cw * xpos
+    val ch = canvas.getHeight()
+    val y1 = ch * ypos
+    val x = x1 - image.getWidth() / 2.0
+    val y = y1 - image.getHeight() / 2.0
     val graphics = canvas.getGraphics.asInstanceOf[Graphics2D]
-    graphics.drawImage(dest, _xpos.toInt, _ypos.toInt, null)
+    graphics.drawImage(image, x.toInt, y.toInt, null)
     graphics.dispose()
   }
 
@@ -128,4 +136,3 @@ object Main {
 
 
 }
-
